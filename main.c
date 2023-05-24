a2aa041154a3afacef78f38771fe95dba5e8708c
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-24 16:24:26 +0300

    format of main.c file and bus

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+ * main - monty
+ * @argc: number of arguments
+ * @argv: string with monty file
+ *
+ * Return: 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	size_t size;
+	ssize_t nchars_read = 1;
+	stack_t *top = NULL;
+	unsigned int line_number = 0;
+	char *data = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(argv[1], "r");
+	if(!fp)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	bus.file = fp;
+	while (nchars_read > 0)
+	{
+		nchars_read = getline(&data, &size, fp);
+		bus.data = data;
+		line_number++;
+		if (nchars_read > 0)
+		{
+			/* functions come in here */
+		}
+	}
+	fclose(file);
+
+	return (0);
+}
---
+++ push.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * push - add to the stack
+ * @head: stack
+ * @line_number: line number
+ *
+ * Return: nothing
+ */
+void push(stack_t **head, unsigned int line_number)
+{
+	if (argument == NUL)
+	{
+		fprint(stderr, "L%u: usage: push integer\n");
+		exit(EXIT_FAILURE);
+	}
+	:wq
+}
