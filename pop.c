c2eeafaea0ba89cc11313e485ddeb9a977c7e5a6
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-25 13:05:40 -0700

    fixed the logic in pop to handle empty stack

--- pop.c
+++ pop.c
@@ -11,14 +11,17 @@ void f_pop(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (*head)
+	if (*head == NULL)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(bus.file);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-	fclose(bus.file);
-	free_stack(*head);
-	exit(EXIT_FAILURE);
+
+	temp = *head;
+	*head = temp->next;
+	if (temp->next)
+		temp->next->prev = NULL;
+	free(temp);
 }
