3f0760ed50b101f562323e9517cdf2bf154bb466
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-28 15:35:14 -0700

    implemented the stack opcode

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - determines if implementation is a stack
+ * @head: stack
+ * @line_number: line number
+ *
+ * return: Nothing
+ */
+void f_stack(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+	bus.st_or_que = 0;
+}
