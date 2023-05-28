477ea437f93b4498bf7b0be3df38aff2d8160560
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-28 15:34:40 -0700

    implemented the queue opcode

---
+++ queue.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_queue - determines if implementation is a quene
+ * @head: stack
+ * @line_number: line number
+ *
+ * return: Nothing
+ */
+void f_queue(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+	bus.st_or_que = 1;
+}
